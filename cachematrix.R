520ef616194f7e66189697078cffe73a71c3d156
gramucse <g.ramucse@gmail.com>
2020-05-07 13:39:03 +0530

    Submit Problem

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+get<-function()x
+print(x)
+list(get=get)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        d<-x$get()
+        
+        R=solve(d)
+       R
 }
